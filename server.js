2a8c05fe43d638615a1f7c5a99707f2096bc1860
Dilpreet360Circle <dilpreet@360cicleinfotech.com>
2023-04-19 13:11:34 +0530

    new

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express'; 
+import fs from 'fs';
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+
+
+
+app.get('/:username/:websitename/:id',(req, res)=>{
+        
+        const a = req.params.username;
+        const b = req.params.websitename;
+        const c = req.params.id;
+      const fileExists = fs.existsSync(`./views/${a}/${b}/${c}.ejs`);
+if(fileExists){
+
+    res.render(`./${a}/${b}/${c}`);
+}else{
+    res.render(`./${a}/${b}/error`);
+
+}
+
+});
+
+app.get('*',(req, res)=>{
+    res.render(`error`);
+});
+app.listen(3001);
